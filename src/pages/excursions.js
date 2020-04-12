8a3c2f2068993457a413aca387e5b090052893db
Mateusz <mateusz.poprawa@gmail.com>
2020-04-12 07:35:46 +0200

    create pages

---
+++ src/pages/excursions.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const excursions = () => {
+  return <div>Excursions page</div>
+}
+
+export default excursions
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    Hello world!
+    <Link to="/">Home</Link>
+    <Link to="/excursions/">Excursions</Link>
+    <Link to="/blog/">Blog</Link>
+    <Link to="/contact/">Contact</Link>
+  </div>
+)
